f586ffbca55250e12a63793a8936a41f26fb9727
DESKTOP-6AB98P2\Admin <lequocviethk19@gmail.com>
2022-05-22 17:38:44 +0700

    initial environment for aave

--- Aave_brownie_py/scripts/get_weth.py
+++ Aave_brownie_py/scripts/get_weth.py
@@ -1,3 +1,8 @@
+from distutils.command.config import config
+from scripts.helpful_script import get_account
+from brownie import interface, config, network
+
+
 def main():
     get_weth()
 
@@ -9,4 +14,9 @@ def get_weth():
     """
     # ABI
     # Address
+
+    account = get_account()
+    weth = interface.IWeth(config["networks"][network.show_active()]["wet_token"])
+    tx = weth.deposit({"from": account, "value": 0.1 * 10**18})
+    print("received 0.1 WETH")
     pass
