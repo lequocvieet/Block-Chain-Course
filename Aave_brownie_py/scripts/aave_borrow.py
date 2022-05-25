6fe201ed0d7b91d69c6b12ef2fa5973e8622cf06
DESKTOP-6AB98P2\Admin <lequocviethk19@gmail.com>
2022-05-25 22:51:42 +0700

    add IlendingPool interface

--- Aave_brownie_py/scripts/aave_borrow.py
+++ Aave_brownie_py/scripts/aave_borrow.py
@@ -1,4 +1,4 @@
-from brownie import network, config,interface
+from brownie import network, config, interface
 from scripts.helpful_script import LOCAL_BLOCKCHAIN_ENVIRONMENTS, get_account
 from scripts.get_weth import get_weth
 
@@ -8,11 +8,20 @@ def main():
     erc20_address = config["networks"][network.show_active()]["weth_token"]
     if network.show_active() in ["mainnet-fork"]:
         get_weth()
+
     # ABI
     # Address
     lending_pool = get_lending_pool()
 
+
 def get_lending_pool():
     # ABI
     # Address
-    lending_pool_addresses_provider=  interface.
+    lending_pool_addresses_provider = interface.ILendingPoolAddressesProvider(
+        config["networks"][network.show_active()]["lending_pool_addresses_provider"]
+        # create lending pool provider interface
+    )
+    # get lending pool address
+    lending_pool_address = lending_pool_addresses_provider.getLendingPool()
+    # ABI
+    # Address-check!
